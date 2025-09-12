cee0c417f31e7c37bbdf368a4fda32604664b14a
JasonAraujo1 <jasonaraujo123@hotmail.com>
2025-09-12 19:35:04 -0300

    first commit

---
+++ calc.rb
@@ -0,0 +1,11 @@
+print "digite o primeiro numero inteiro: "
+
+num1 = gets.chomp.to_i
+
+print "digite o segundo numero inteiro: "
+
+num2 = gets.chomp.to_i
+
+soma  = num1 + num2
+
+puts "ao valor da soma é #{soma}"
