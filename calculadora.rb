9ead32e4d2ae5e424fb97ed806e719416db99c94
Jason Araújo <jason@email.com>
2025-09-13 14:48:50 -0300

    adiciona implementação da calculadora básica

---
+++ calculadora.rb
@@ -0,0 +1,26 @@
+print 'Escolha um número: '
+num1 = gets.chomp.to_i
+
+print 'Escolha outro número: '
+num2 = gets.chomp.to_i
+
+print 'escolha uma operação (+, -, *, /): '
+
+operacao = gets.chomp
+
+case operacao
+    when '+'
+        res = num1 + num2
+    when '-'
+        res = num1 - num2   
+    when '*'
+        res = num1 * num2
+    when '/'
+        res = num1 / num2
+    else
+        puts 'Operação inválida'
+        exit
+end
+
+
+puts "o resultado é: #{res}"
