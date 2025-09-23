3d27ebb95ab95b14b0c91891ef639e48eb125370
JasonAraujo1 <jasonaraujo123@hotmail.com>
2025-09-23 12:21:46 -0300

    refatora a classe Calculadora e melhora a interação com o usuário

--- desafioCalculadora.rb
+++ desafioCalculadora.rb
@@ -1,32 +1,57 @@
 
+class Calculadora
+    def soma(num1, num2)
+        res = num1 + num2
+        puts "o resultado é #{res}"
+        return res
+    end
+    def subtracao(num1, num2)
+        res = num1 - num2
+        puts "o resultado é #{res}"
+        return res
+    end
+    def divisao(num1, num2)
+        res = num1 / num2
+        puts "o resultado é #{res}"
+        return res
+    end
+    def multiplicacao(num1, num2)
+        res = num1 * num2
+        puts "o resultado é #{res}"
+        return res
+    end   
+end
+
+calc = Calculadora.new
+
 res = 0
+
 while true
-    if res == 0
-        puts "escolha um número..."
+    if res == 0 
+        puts "escolha um número"
         num1 = gets.chomp.to_f
     else
+        puts "resultado anterior #{res}"
         num1 = res
-        puts "resultado anterior: #{num1}"
     end
 
-    puts "escolha um operador(+,-,/,*)..."
+    puts "escolha um operador"
     operador = gets.chomp
 
-    puts "escolha outro numero..."
+    puts "escolha outro número"
     num2 = gets.chomp.to_f
 
     case operador
-    when "+"
-        res = num1 + num2
-    when "-"
-        res = num1 - num2
-    when "/"
-        res = num1 / num2
-    when "*"
-        res = num1 * num2
-    else
-        puts "operação inválida"
-        exit
+        when  "+"
+            res = calc.soma(num1, num2)
+        when "-"
+            res = calc.subtracao(num1, num2)
+        when "/"
+            res = calc.divisao(num1, num2)
+        when "*"
+            res = calc.multiplicacao(num1, num2)
+        else
+            puts "operação inválida"
+            exit
     end
-    puts "o resuldado é: #{res}"
 end
