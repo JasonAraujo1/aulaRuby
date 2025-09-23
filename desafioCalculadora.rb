1a56b270efd5174e9d252482ec37e5d085bc2d10
JasonAraujo1 <jasonaraujo123@hotmail.com>
2025-09-23 13:25:54 -0300

    refatora a classe DesafioCalculadora e simplifica a lógica de operações

--- desafioCalculadora.rb
+++ desafioCalculadora.rb
@@ -1,57 +1,54 @@
 
-class Calculadora
-    def soma(num1, num2)
-        res = num1 + num2
-        puts "o resultado é #{res}"
-        return res
-    end
-    def subtracao(num1, num2)
-        res = num1 - num2
-        puts "o resultado é #{res}"
-        return res
-    end
-    def divisao(num1, num2)
-        res = num1 / num2
-        puts "o resultado é #{res}"
-        return res
-    end
-    def multiplicacao(num1, num2)
-        res = num1 * num2
-        puts "o resultado é #{res}"
-        return res
-    end   
-end
+class DesafioCalculadora
+    
+    def calculadora(num1, num2, operador)
+        case operador
+        when "+"
+            res = num1 + num2
 
-calc = Calculadora.new
-
-res = 0
-
-while true
-    if res == 0 
-        puts "escolha um número"
-        num1 = gets.chomp.to_f
-    else
-        puts "resultado anterior #{res}"
-        num1 = res
-    end
-
-    puts "escolha um operador"
-    operador = gets.chomp
-
-    puts "escolha outro número"
-    num2 = gets.chomp.to_f
-
-    case operador
-        when  "+"
-            res = calc.soma(num1, num2)
         when "-"
-            res = calc.subtracao(num1, num2)
+            res = num1 - num2
+
         when "/"
-            res = calc.divisao(num1, num2)
+            res = num1 / num2
+
         when "*"
-            res = calc.multiplicacao(num1, num2)
+            res = num1 * num2
         else
             puts "operação inválida"
             exit
+        end
+
+        puts "o resultado é #{res}"
+        return res
     end
+
+    def dados
+        res = 0
+
+        loop do
+            if res == 0 
+                puts "escolha um número"
+                num1 = gets.chomp.to_f
+            else
+                puts "resultado anterior #{res}"
+                num1 = res
+            end
+
+            puts "escolha um operador"
+            operador = gets.chomp
+
+            puts "escolha outro número"
+            num2 = gets.chomp.to_f
+
+            res = calculadora(num1,num2,operador)
+        end
+
+    end
+
 end
+    
+DesafioCalculadora.new.dados
+
+
+
