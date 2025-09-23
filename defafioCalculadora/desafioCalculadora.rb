16487a319ed0fb585f7fadfacdc42bc938f83c09
JasonAraujo1 <jasonaraujo123@hotmail.com>
2025-09-23 16:48:15 -0300

    refatora a classe DesafioCalculadora para incluir métodos de validação de operador e número

--- defafioCalculadora/desafioCalculadora.rb
+++ defafioCalculadora/desafioCalculadora.rb
@@ -1,7 +1,9 @@
 
 class DesafioCalculadora
 
+    def self.init
        @res = 0.0
+    end
 
     def self.calculadora(num1, num2, operador)
         case operador
@@ -21,31 +23,65 @@ class DesafioCalculadora
             exit
         end
 
-        puts "o resultado é #{@res}"
+        self.resultado
         return @res
     end
 
+    def self.validacaoOperador(operador)
+        if ["+","-","*","/"].include?(operador)
+            operador
+        else
+            puts "operador inválido"
+            nil
+        end
+    end
+
+    def self.validacaoNumero(valor)
+        if valor.strip.empty?
+            puts "número vazio"
+            return nil
+        end
+
+        num = valor.to_f
+        if num < 0
+            puts "número negativo"
+            return nil
+        end
+
+        num
+    end
+
     def self.dados
+        self.init
         loop do
-            if @res == 0 
+            if @res == 0
                 puts "escolha um número"
-                num1 = gets.chomp.to_f
+                entrada1 = gets.chomp             
+                num1 = self.validacaoNumero(entrada1)
+                next if num1.nil?                 
             else
                 puts "resultado anterior #{@res}"
                 num1 = @res
             end
-
+          
             puts "escolha um operador"
             operador = gets.chomp
 
+            operador = self.validacaoOperador(operador)
+            next if operador.nil?
+
             puts "escolha outro número"
-            num2 = gets.chomp.to_f
+            entrada2 = gets.chomp
+            num2 = self.validacaoNumero(entrada2)
+            next if num2.nil?
 
             @res = self.calculadora(num1,num2,operador)
         end
-
     end
 
+    def self.resultado
+        puts "o resultado é #{@res}"
+    end
 end
     
 DesafioCalculadora.dados
