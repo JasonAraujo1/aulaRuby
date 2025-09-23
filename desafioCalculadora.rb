15e5b2605c6f8d2004162cd419e2ee9b3102b140
JasonAraujo1 <jasonaraujo123@hotmail.com>
2025-09-22 22:39:12 -0300

    corrige a entrada de números para ponto flutuante na calculadora

--- desafioCalculadora.rb
+++ desafioCalculadora.rb
@@ -1,12 +1,19 @@
+
+res = 0
 while true
-    puts "escolha um número..."
-    num1 = gets.chomp.to_i
+    if res == 0
+        puts "escolha um número..."
+        num1 = gets.chomp.to_f
+    else
+        num1 = res
+        puts "resultado anterior: #{num1}"
+    end
 
     puts "escolha um operador(+,-,/,*)..."
     operador = gets.chomp
 
     puts "escolha outro numero..."
-    num2 = gets.chomp.to_i
+    num2 = gets.chomp.to_f
 
     case operador
     when "+"
