19e2ee5016b1ba8aec03b61e4fd471d563c28f50
JasonAraujo1 <jasonaraujo123@hotmail.com>
2025-09-24 22:34:39 -0300

    remove arquivos de exemplo e códigos não utilizados

--- exerc3.rb
+++
@@ -1,8 +0,0 @@
-def mat(num, pot)
-    res = num ** pot
-    puts "o resultado é #{res}"
-    
-end
-
-puts mat(3,2)
-
--- names.rb
+++
@@ -1,7 +0,0 @@
-print "qual sua idade?"
-idade = gets.chomp.to_i
-
-print "qual seu nome?"
-nome = gets.chomp
-
-puts "#{nome} #{idade}"
